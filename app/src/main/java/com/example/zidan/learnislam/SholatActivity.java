14bc3d4442c5ce7695408022c99e062c780b5c71
lutzid <lut.zidan@gmail.com>
2020-04-03 00:38:04 +0700

    add panduan sholat feature

--- app/src/main/java/com/example/zidan/learnislam/SholatActivity.java
+++ app/src/main/java/com/example/zidan/learnislam/SholatActivity.java
@@ -2,6 +2,7 @@ package com.example.zidan.learnislam;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 
 public class SholatActivity extends AppCompatActivity {
 
@@ -9,5 +10,19 @@ public class SholatActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sholat);
+
+        getSupportActionBar().setTitle("Panduan Sholat");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+        if (id == android.R.id.home) {
+            finish();
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
